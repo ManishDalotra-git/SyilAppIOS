e81a265753cbfb59d72f1b40021e76b60ec4107d
Manish Dalotra <manish.dalotra@techstriker.com>
2026-08-13 18:44:44 +0530

    server.js file

--- src/screens/Chatscreen.jsx
+++ src/screens/Chatscreen.jsx
@@ -40,7 +40,7 @@ const Chatscreen = () => {
           />
         </TouchableOpacity>
 
-        <Text style={styles.headerTitle}>Ask Alex</Text>
+        <Text style={styles.headerTitle}>Ask SYIL</Text>
       </View>
 
       {/* LOADING OVERLAY */}
